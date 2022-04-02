678f4cda206a720bc984d0ff91fd901aab2cb889
eswarachari990 <100748879+eswarachari990@users.noreply.github.com>
2022-04-02 16:46:38 +0530

    Add files via upload

---
+++ lockedme/src/lockedme/Test.java
@@ -0,0 +1,13 @@
+package lockedme;
+
+public class Test {
+	
+       public static final String path = "C:/Users/sandr/eclipse-workspace/lockedme/locker"; 
+	
+	     public static void main(String[] args) {
+		    Display menu = new Display();
+		    menu.introScreen();
+		    menu.mainMenu();
+	}
+
+}
